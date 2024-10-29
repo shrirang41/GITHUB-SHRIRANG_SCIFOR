f7a47fdd34dd093506c1ba82044dd9df7f2b3c7c
shrirang41 <shrirangn8@gmail.com>
2024-10-29 20:47:21 +0530

    Create primitive.js

---
+++ DailyTask/primitive.js
@@ -0,0 +1,17 @@
+
+let myString = "Hello, world!";  
+let myNumber = 42;               
+let myBoolean = true;            
+let myNull = null;               
+let myUndefined;                 
+let myBigInt = 1234567890123456789012345678901234567890n; 
+let mySymbol = Symbol("symbol"); 
+
+console.log(typeof myString);    
+console.log(typeof myNumber);    
+console.log(typeof myBoolean);   
+console.log(typeof myNull);     
+console.log(typeof myUndefined); 
+console.log(typeof myBigInt);    
+console.log(typeof mySymbol);    
+
